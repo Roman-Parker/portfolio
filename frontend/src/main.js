74a4dc1e56891ab99106237ab8c52ebe8621c042
Roman Parker <roman@parker.pm>
2025-02-23 18:43:30 +0100

    Switched store from vuex to pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-
 import "./index.css"
+import {createPinia} from 'pinia'
 
-createApp(App).use(store).use(router).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).mount('#app')
