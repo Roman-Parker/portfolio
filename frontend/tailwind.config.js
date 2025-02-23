a5ea77e4dcb65a3746bfb7b60c6cf9b0a5b780eb
Roman Parker <roman@parker.pm>
2025-02-23 09:14:59 +0100

    Fixed indentation in tailwindcss config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        darkSlate: '#0F172A',
-        gunmetal: '#141A1F',
-        turquoise: {
-          DEFAULT: '#14B8A6',
-          light: '#2DD4BF',
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                darkSlate: '#0F172A',
+                gunmetal: '#141A1F',
+                turquoise: {
+                    DEFAULT: '#14B8A6',
+                    light: '#2DD4BF',
+                },
+                coolGray: '#9CA3AF',
+                offWhite: '#F8FAFC',
+            },
         },
-        coolGray: '#9CA3AF',
-        offWhite: '#F8FAFC',
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
