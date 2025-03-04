24f0945e3d0a9467eb8744e6ff56d57b5f348373
Roman Parker <roman@parker.pm>
2025-03-04 20:49:13 +0100

    added button component

---
+++ frontend/src/components/DownloadResumeButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button
+        @click="downloadResume"
+        class="inline-block px-6 py-3
+                border-2 border-turquoise
+                text-turquoise
+                font-semibold rounded
+                transition-colors
+                hover:bg-turquoise
+                hover:text-gunmetal"
+        >
+        Resume
+        </button>
+  </template>
+
+<script setup lang="ts">
+function downloadResume(): void {
+    const link = document.createElement('a');
+    link.href = '/public/roman_parker_resume.pdf';
+    link.setAttribute('download', 'roman_parker_resume.pdf');
+    link.click();
+}
+</script>
