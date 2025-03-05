b19d14dc84db26b3be7034843dac2d8653e9df53
Roman Parker <roman@parker.pm>
2025-03-05 19:34:22 +0100

    added typed langauges by implementing i18n

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import "./index.css"
 import {createPinia} from 'pinia'
+import i18n from './i18n.js'
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.use(i18n)
+app.mount('#app')
